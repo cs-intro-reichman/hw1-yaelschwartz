1128803e8b5dd070313367d47ecc35baf7a87d6c
Yael Schwartz <yaelschwartz@Yaels-MacBook-Air.local>
2025-11-05 16:41:32 +0200

    third commit

--- Bill3.java
+++ Bill3.java
@@ -6,6 +6,6 @@ public class Bill3 {
 	    String name2 = args[1];
 		String name3 = args[0];
 		double val = Double.parseDouble(args[3]);
-		    System.out.println("Dear " + name1 + " " + name2 + " and " + name3 + ": pay " + (Math.ceil(val/3)) + " Shekels each");
+		    System.out.println("Dear " + name1 + ", " + name2 + ", and " + name3 + ": pay " + (Math.ceil(val/3)) + " Shekels each");
     }
 }
